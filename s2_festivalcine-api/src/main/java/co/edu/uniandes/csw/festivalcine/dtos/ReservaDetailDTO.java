6c8ec9c172186ed4aab58c6a2e00a4033658e70c
estudiante <estudiante@ISIS2603-359.sis.virtual.uniandes.edu.co>
2018-08-30 19:00:00 -0500

    "Se realizaron cambios en las entidades Reserva y usuario a nivel de Dtos y recursos"

--- s2_festivalcine-api/src/main/java/co/edu/uniandes/csw/festivalcine/dtos/ReservaDetailDTO.java
+++ s2_festivalcine-api/src/main/java/co/edu/uniandes/csw/festivalcine/dtos/ReservaDetailDTO.java
@@ -5,9 +5,9 @@
  */
 package co.edu.uniandes.csw.festivalcine.dtos;
 
-import co.edu.uniandes.csw.festivalcine.entities.SillaEntity;
-import co.edu.uniandes.csw.festivalcine.entities.FuncionEntity;
-import co.edu.uniandes.csw.festivalcine.entities.ReservaEntity;
+//import co.edu.uniandes.csw.festivalcine.entities.SillaEntity;
+//import co.edu.uniandes.csw.festivalcine.entities.FuncionEntity;
+//import co.edu.uniandes.csw.festivalcine.entities.ReservaEntity;
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,7 +22,7 @@ public class ReservaDetailDTO extends ReservaDTO implements Serializable
     /**
      * Lista de tipo CalificacionDTO, contiene las calificaciones asociadas con el critico
      */
-    private List<SillaDTO> sillas;
+    //private List<SillaDTO> sillas;
 
    
     
@@ -36,73 +36,73 @@ public class ReservaDetailDTO extends ReservaDTO implements Serializable
     /**
      * Constructor por defecto
      */
-    public ReservaDetailDTO(){}
+    //public ReservaDetailDTO(){}
     
     /**
      * Constructor para transformar un Entity a un DTO
      * @param reservaEntity La entidad del critico para transformar a DTO.
      */
-    public ReservaDetailDTO(ReservaEntity reservaEntity)
+    public ReservaDetailDTO() //ReservaEntity reservaEntity)
     {
-        super(reservaEntity);
-        if(reservaEntity != null)
-        {
-            if(reservaEntity.darSillas() != null)
-            {
-                sillas = new ArrayList<SillaDTO>();
-                for(SillaEntity entitySilla : reservaEntity.darSillas())
-                {
-                    sillas.add(new SillaDTO(entitySilla));
-                }
-            }
-            
-            if(reservaEntity.darFunciones() != null)
-            {
-                funciones = new ArrayList<FuncionDTO>();
-                for(FuncionDTO entityFuncion : reservaEntity.darFunciones())
-                {
-                    funciones.add(new FuncionDTO(entityFuncion));
-                }
-            }
-        }
+//        super(reservaEntity);
+//        if(reservaEntity != null)
+//        {
+//            if(reservaEntity.darSillas() != null)
+//            {
+//                sillas = new ArrayList<SillaDTO>();
+//                //for(SillaEntity entitySilla : reservaEntity.darSillas())
+//                {
+//                    //sillas.add(new SillaDTO(entitySilla));
+//                }
+//            }
+//            
+//            if(reservaEntity.darFunciones() != null)
+//            {
+//                funciones = new ArrayList<FuncionDTO>();
+//                //for(FuncionDTO entityFuncion : reservaEntity.darFunciones())
+//                {
+//                    //funciones.add(new FuncionDTO(entityFuncion));
+//                }
+//            }
+//        }
     }
     
     /**
      * Transforma un DTO a un Entity
      * @return El DTO del critico para transformar a Entity
      */
-    @Override
-    public ReservaEntity toEntity()
-    {
-        ReservaEntity reservaEntity = super.toEntity();
-        if(sillas != null)
-        {
-            List<SillaEntity> sillaEntity =  new ArrayList<>();
-            for(SillaDTO dtoSilla : sillas)
-            {
-                sillaEntity.add(dtoSilla.toEntity());
-            }
-            reservaEntity.setReserva(sillaEntity);
-        }
-        if(funciones != null)
-        {
-           List<FuncionEntity> funcionEntity = new ArrayList();
-           for(FuncionDTO dtoFuncion : funciones)
-           {
-               funcionEntity.add(dtoFuncion.toEntity());
-           }
-            reservaEntity.setFunciones(funcionEntity);
-        }
-        return reservaEntity;
-    }
+//    @Override
+//    public ReservaEntity toEntity()
+//    {
+//        ReservaEntity reservaEntity = super.toEntity();
+//        if(sillas != null)
+//        {
+//            //List<SillaEntity> sillaEntity =  new ArrayList<>();
+//            for(SillaDTO dtoSilla : sillas)
+//            {
+//                //sillaEntity.add(dtoSilla.toEntity());
+//            }
+//            //reservaEntity.setReserva(sillaEntity);
+//        }
+//        if(funciones != null)
+//        {
+//           //List<FuncionEntity> funcionEntity = new ArrayList();
+//           for(FuncionDTO dtoFuncion : funciones)
+//           {
+//               //funcionEntity.add(dtoFuncion.toEntity());
+//           }
+//            //reservaEntity.setFunciones(funcionEntity);
+//        }
+//        return reservaEntity;
+//    }
     
-     public List<SillaDTO> getSillas() {
-        return sillas;
-    }
-
-    public void setSillas(List<SillaDTO> sillas) {
-        this.sillas = sillas;
-    }
+//     public List<SillaDTO> getSillas() {
+//        return sillas;
+//    }
+//
+//    public void setSillas(List<SillaDTO> sillas) {
+//        this.sillas = sillas;
+   // }
 
     public List getFunciones() {
         return funciones;
