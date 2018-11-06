e2314e33fa383ce9e68a6c50d28e83109af2c914
estudiante <mj.moya@uniandes.edu.co>
2018-11-06 15:09:52 -0500

    Correción sala

--- s2_festivalcine-api/src/main/java/co/edu/uniandes/csw/festivalcine/resources/SalaResource.java
+++ s2_festivalcine-api/src/main/java/co/edu/uniandes/csw/festivalcine/resources/SalaResource.java
@@ -14,6 +14,7 @@ import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -35,7 +36,7 @@ import javax.ws.rs.WebApplicationException;
 public class SalaResource {
     private static final Logger LOGGER = Logger.getLogger(SalaResource.class.getName());
 
-    
+    @Inject
     SalaLogic salaLogic; // Variable para acceder a la lógica de la aplicación. Es una inyección de dependencias.
 
     /**
