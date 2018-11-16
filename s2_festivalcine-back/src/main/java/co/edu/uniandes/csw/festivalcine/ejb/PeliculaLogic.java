08ef930cdde2a9a02c4b5d7b9e93401c5b1e3775
Paula Velandia <pa.velandiar@uniandes.edu.co>
2018-11-16 08:08:34 -0500

    DT pelicula logic

--- s2_festivalcine-back/src/main/java/co/edu/uniandes/csw/festivalcine/ejb/PeliculaLogic.java
+++ s2_festivalcine-back/src/main/java/co/edu/uniandes/csw/festivalcine/ejb/PeliculaLogic.java
@@ -7,7 +7,6 @@ package co.edu.uniandes.csw.festivalcine.ejb;
 
 //IMPORTS
 import co.edu.uniandes.csw.festivalcine.entities.PeliculaEntity;
-import co.edu.uniandes.csw.festivalcine.exceptions.BusinessLogicException;
 import co.edu.uniandes.csw.festivalcine.persistence.PeliculaPersistence;
 import java.util.List;
 import java.util.logging.Level;
@@ -29,14 +28,14 @@ public class PeliculaLogic {
    @Inject
    private PeliculaPersistence persistence;
    
-   public PeliculaEntity createPelicula(PeliculaEntity pelicula)throws BusinessLogicException{
+   public PeliculaEntity createPelicula(PeliculaEntity pelicula){
      LOGGER.info("Inicia la creación de una Pelicula");  
      
      persistence.create(pelicula);
      LOGGER.info("Finaliza la creación de una Pelicula");
      return pelicula;
    }
-   public PeliculaEntity findById(long id)throws WebApplicationException{
+   public PeliculaEntity findById(long id){
        PeliculaEntity peliculaBuscada= persistence.findById(id);
        
        //SE VERIFICA SI EXISTE UNA PELICULA SI ES NULL MANDA ERROR
@@ -72,7 +71,7 @@ public class PeliculaLogic {
     public List<PeliculaEntity> findByDirector( String nombre){
         List<PeliculaEntity> peliculasBuscadas= persistence.findByDirector(nombre);
         
-        if (peliculasBuscadas.isEmpty()==true){
+        if (peliculasBuscadas.isEmpty()){
             throw new WebApplicationException("No existe la pelicula con el director"+ nombre, 404);
         }
         
@@ -82,7 +81,7 @@ public class PeliculaLogic {
    public List<PeliculaEntity> findByDuracion( int duracion){
         List<PeliculaEntity> peliculasBuscadas= persistence.findByDuracion(duracion);
         
-        if (peliculasBuscadas.isEmpty()==true){
+        if (peliculasBuscadas.isEmpty()){
             throw new WebApplicationException("No existe la pelicula con la duracion"+ duracion, 404);
         }
         
@@ -92,7 +91,7 @@ public class PeliculaLogic {
    public List<PeliculaEntity> findByPuntaje( double puntaje){
         List<PeliculaEntity> peliculasBuscadas= persistence.findByPuntaje(puntaje);
         
-        if (peliculasBuscadas.isEmpty()==true){
+        if (peliculasBuscadas.isEmpty()){
             throw new WebApplicationException("No existe la pelicula con el puntaje"+ puntaje, 404);
         }
         
@@ -102,7 +101,7 @@ public class PeliculaLogic {
    public List<PeliculaEntity> findByPais( String pais){
         List<PeliculaEntity> peliculasBuscadas= persistence.findByPais(pais);
         
-        if (peliculasBuscadas.isEmpty()==true){
+        if (peliculasBuscadas.isEmpty()){
             throw new WebApplicationException("No existe la pelicula con el pais"+ pais, 404);
         }
         
@@ -121,7 +120,7 @@ public class PeliculaLogic {
       return persistence.update(peli);
    }
    
-    public void delete(Long id) throws WebApplicationException, BusinessLogicException{
+    public void delete(Long id){
          LOGGER.log(Level.INFO, "Inicia proceso de borrar una pelicula con id={0}", id);
          persistence.delete(id);
     }
