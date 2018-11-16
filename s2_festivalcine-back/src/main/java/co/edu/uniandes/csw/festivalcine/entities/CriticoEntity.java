82faff44419a6308b49ed3121a4ba01d59e22254
estudiante <mj.moya@uniandes.edu.co>
2018-11-16 08:29:27 -0500

    Cambios critico entity

--- s2_festivalcine-back/src/main/java/co/edu/uniandes/csw/festivalcine/entities/CriticoEntity.java
+++ s2_festivalcine-back/src/main/java/co/edu/uniandes/csw/festivalcine/entities/CriticoEntity.java
@@ -3,6 +3,8 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+
+
 package co.edu.uniandes.csw.festivalcine.entities;
 
 import java.io.Serializable;
